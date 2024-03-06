4846a09476f732405875533cea3ccd7719375f76
Khdulkadir <khdulkadir@gmail.com>
2024-03-06 11:10:27 +0100

    .

---
+++ server.js
@@ -0,0 +1,44 @@
+/*** Express setup & start ***/
+
+// Importeer de zelfgemaakte functie fetchJson uit de ./helpers map
+import fetchJson from "./helpers/fetch-json.js";
+
+// Importeer het npm pakket express uit de node_modules map
+import express, { response } from "express";
+
+// Maak een nieuwe express app aan
+const app = express();
+
+// Stel ejs in als template engine
+app.set("view engine", "ejs");
+
+// Stel de map met ejs templates in
+app.set("views", "./views");
+
+// Gebruik de map 'public' voor statische resources, zoals stylesheets, afbeeldingen en client-side JavaScript
+app.use(express.static("public"));
+
+// Zorgt dat werken met request data makkelijker wordt
+app.use(express.urlencoded({ extended: true }));
+
+// Stel het poortnummer in waar express op moet gaan luisteren
+app.set("port", process.env.PORT || 8000);
+
+// Start express op, haal daarbij het zojuist ingestelde poortnummer op
+app.listen(app.get("port"), function () {
+  // Toon een bericht in de console en geef het poortnummer door
+  console.log(`Application started on http://localhost:${app.get("port")}`);
+});
+
+/*** Routes & data ***/
+// Maak een GET route voor de index
+app.get("/", function (request, response) {
+  // Haal alle artikelen uit de Wordpress API op
+  fetchJson().then((artikelData) => {
+    // apiData bevat gegevens van alle personen uit alle squads
+
+    // Render index.ejs uit de views map en geef de opgehaalde data mee als variabele, genaamd persons
+    // HTML maken op basis van JSON data
+    response.render("index", {});
+  });
+});
